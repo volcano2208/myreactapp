d68b76cc579a10f339bfa8d5bec500685cc53335
Duong Nguyen <kakaphucle@gmail.com>
2023-10-19 23:21:51 +0700

    init nx with react application with nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="myreactapp" />
+      <NxWelcome title="Volcano" />
+      demo
     </div>
   );
 }
